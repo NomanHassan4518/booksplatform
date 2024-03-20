6f01b0a1d1e274001cdf69c9825960b74323f1c0
Hassan <malikhassanhu55@gmail.com>
2024-03-20 17:41:03 +0500

    ssd

---
+++ Backend/collections/config.js
@@ -0,0 +1,4 @@
+const mongoose=require("mongoose");
+// mD0rlyyXe5wOXSEM
+
+mongoose.connect("mongodb://malikhassanhu55:mD0rlyyXe5wOXSEM@ac-relv2ed-shard-00-00.1rhhvjs.mongodb.net:27017,ac-relv2ed-shard-00-01.1rhhvjs.mongodb.net:27017,ac-relv2ed-shard-00-02.1rhhvjs.mongodb.net:27017/?ssl=true&replicaSet=atlas-ne9mje-shard-0&authSource=admin&retryWrites=true&w=majority&appName=library")
--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,25 @@
 const express=require("express");
+const cors=require("cors");
 const app=express();
+
+require("./collections/config")
+let User=require("./collections/user")
+
+app.use(express.json());
+app.use(cors())
 
 app.get("/",(req,resp)=>{
     resp.send("appi is working")
 })
 
+app.post("/register", async (req,resp)=>{
+    let user = User(req.body);
+    let result = await user.save();
+    result = result.toObject();
+    delete result.password
+    resp.send(result)
+})
+
 app.listen(5000,()=>{
     console.log("working");
-})
+});
